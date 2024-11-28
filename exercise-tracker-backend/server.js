425c8e312671e258e33a2d96e29bc503090f651f
Shivaraj S <shivarajskit@gmail.com>
2024-11-28 17:09:42 +0530

    Fixing error backend from returning 404 page for every route

---
+++ exercise-tracker-backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import exerciseRoutes from './routes/exerciseRoutes.js'; 
+import userRoutes from './routes/userRoutes.js'; 
+import cors from 'cors';
+
+
+const app = express();
+app.use(express.json());
+app.use(cors()); 
+
+app.use('/api/exercises', exerciseRoutes);
+app.use('/api/users', userRoutes);
+app.use('*', express.static('public')); // display 404 html page from public folder if accessed 5000 port from browser
+// app.use(logger);  // middleware between requests and responses
+console.log("run: 'npm run devStart' to auto restart server");
+// Add your other routes (e.g., exercises routes) here
+
+function logger(req, res, next){
+  console.log(req.originalUrl);
+  next();
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
